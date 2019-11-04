620611501358648973db2e5e7f4d9bbe6c9e2a85
Vagabondan <anj5@mail.ru>
2019-11-04 10:10:37 +0300

    Change owner

--- build_nginx-proxy_ubuntu-bionic.sh
+++ build_nginx-proxy_ubuntu-bionic.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 os="ubuntu-bionic"
-images="base runit nginx nginx-mailer nginx-cron nginx-proxy"
+images="base runit nginx nginx-mailer nginx-cron"
+target_image=nginx-proxy
+owner=${3:-vagabondan}
 
 for img in $images 
 do
   ./build_common.sh $img $os
 done
+
+./build_common.sh ${target_image} $os $owner
+docker push ${owner}/${target_image}
